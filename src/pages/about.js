94471b89baa751da3987be2f85d3feee03144ff4
Dero <106393@gmail.com>
2020-02-07 14:32:11 +0400

    Remove default styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,13 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <p>Home</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="About" />
+    <h1>Hi people</h1>
+    <p>About</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
   </Layout>
 )
 
