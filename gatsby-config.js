5119fd0711157a606ed7e370f3ec7962c7e589e0
Dero <106393@gmail.com>
2020-02-10 15:59:11 +0400

    gatsby-source-google-sheet, dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,6 @@
-module.exports = {
+require('dotenv').config();
+
+  module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -6,6 +8,9 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    'gatsby-transformer-remark',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -20,8 +25,6 @@ module.exports = {
         path: `${__dirname}/src/pages`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -34,6 +37,21 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-transformer-remark'
+    {
+    resolve: `gatsby-source-google-sheet`,
+    options: {
+    // For protected spreadsheets you can use two-legged OAuth as described here:
+    // https://www.npmjs.com/package/google-spreadsheet#service-account-recommended-method
+    creds: {
+      client_email: process.env.GS_CLIENT_EMAIL,
+       private_key: process.env.GS_PRIVATE_KEY
+    },
+    // This is the bit after "/d/" and before "/edit" in the URL of a
+    // Google Sheets document. I.e.,
+    // https://docs.google.com/spreadsheets/d/1ec1bO25bbEL4pdZjhlV3AppMtnO65D0ZI8fXy4z47Dw/edit#gid=0
+    spreadsheetKey: `1AwTb93M0l6fH6J4mXOiA9AavAhNS71GRvInrnJI79mo`,
+      rootName: "GoToEurope" // default is Sheet
+  }
+  }
   ],
 }
