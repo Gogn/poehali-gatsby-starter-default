458aeb70d0b5d340927265f6148cca17edc2935f
Dero <106393@gmail.com>
2020-02-10 22:19:32 +0400

    Revert "gatsby-source-google-sheet, dotenv"
    This reverts commit 5119fd07

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,4 @@
-require('dotenv').config();
-
-  module.exports = {
+module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -8,9 +6,6 @@ require('dotenv').config();
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    'gatsby-transformer-remark',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -25,6 +20,8 @@ require('dotenv').config();
         path: `${__dirname}/src/pages`,
       },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -37,21 +34,6 @@ require('dotenv').config();
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-    resolve: `gatsby-source-google-sheet`,
-    options: {
-    // For protected spreadsheets you can use two-legged OAuth as described here:
-    // https://www.npmjs.com/package/google-spreadsheet#service-account-recommended-method
-    creds: {
-      client_email: process.env.GS_CLIENT_EMAIL,
-       private_key: process.env.GS_PRIVATE_KEY
-    },
-    // This is the bit after "/d/" and before "/edit" in the URL of a
-    // Google Sheets document. I.e.,
-    // https://docs.google.com/spreadsheets/d/1ec1bO25bbEL4pdZjhlV3AppMtnO65D0ZI8fXy4z47Dw/edit#gid=0
-    spreadsheetKey: `1AwTb93M0l6fH6J4mXOiA9AavAhNS71GRvInrnJI79mo`,
-      rootName: "GoToEurope" // default is Sheet
-  }
-  }
+    'gatsby-transformer-remark'
   ],
 }
